55dd6647fef5118d60d6e24208f5a7e1eb9f4a95
Iblis Lin <e196819@hotmail.com>
2016-04-12 21:35:24 +0800

    [app] config host and port from cmd argv

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 import cherrypy
+import sys
 
 from jinja2 import Environment, FileSystemLoader
 from ws4py.server.cherrypyserver import WebSocketPlugin, WebSocketTool
 
 
-cherrypy.config.update({
-    'server.socket_host': '0.0.0.0',
-    'server.socket_port': 5005
-})
 env = Environment(loader=FileSystemLoader('templates'))
 WebSocketPlugin(cherrypy.engine).subscribe()
 cherrypy.tools.websocket = WebSocketTool()
@@ -30,4 +27,8 @@ class YouTubeRemote:
 
 
 if __name__ == '__main__':
+    cherrypy.config.update({
+        'server.socket_host': sys.argv[1] if len(sys.argv) > 1 else '0.0.0.0',
+        'server.socket_port': sys.argv[2] if len(sys.argv) > 2 else 5005
+    })
     cherrypy.quickstart(YouTubeRemote(), config='app.conf')
